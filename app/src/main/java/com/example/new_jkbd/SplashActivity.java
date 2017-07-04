9af29669bc198c0e612b4edab17dbb8abb26153d
969474010 <969474010@qq.com>
2017-07-04 20:42:10 +0800

    添加闪屏并设置闪屏背景图片及去掉APP所有的主题

---
+++ app/src/main/java/com/example/new_jkbd/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.example.new_jkbd;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by &&&&& on 2017/7/4.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+}
