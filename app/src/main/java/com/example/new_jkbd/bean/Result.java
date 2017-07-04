4d79f58ecc7fd2b5680488d69b6b783a7da47b4c
969474010 <969474010@qq.com>
2017-07-04 21:44:54 +0800

    添加三个实体类

---
+++ app/src/main/java/com/example/new_jkbd/bean/Result.java
@@ -0,0 +1,140 @@
+package com.example.new_jkbd.bean;
+
+import java.util.List;
+
+/**
+ * Created by &&&&& on 2017/7/4.
+ */
+
+public class Result {
+
+    /**
+     * error_code : 0
+     * reason : ok
+     * result : [{"id":4,"question":"这个标志是何含义？","answer":"2","item1":"禁止自行车通行车道","item2":"非机动车车道","item3":"自行车专用车道","item4":"停放自行车路段","explains":"此图为非机动车车道，别误以为自行车专用车道，没有自行车专用车道这一说。","url":"http"}]
+     */
+
+    private int error_code;
+    private String reason;
+    private List<Exam> result;
+
+    public int getError_code() {
+        return error_code;
+    }
+
+    public void setError_code(int error_code) {
+        this.error_code = error_code;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public List<Exam> getResult() {
+        return result;
+    }
+
+    public void setResult(List<Exam> result) {
+        this.result = result;
+    }
+
+    public static class ResultBean {
+        /**
+         * id : 4
+         * question : 这个标志是何含义？
+         * answer : 2
+         * item1 : 禁止自行车通行车道
+         * item2 : 非机动车车道
+         * item3 : 自行车专用车道
+         * item4 : 停放自行车路段
+         * explains : 此图为非机动车车道，别误以为自行车专用车道，没有自行车专用车道这一说。
+         * url : http
+         */
+
+        private int id;
+        private String question;
+        private String answer;
+        private String item1;
+        private String item2;
+        private String item3;
+        private String item4;
+        private String explains;
+        private String url;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getQuestion() {
+            return question;
+        }
+
+        public void setQuestion(String question) {
+            this.question = question;
+        }
+
+        public String getAnswer() {
+            return answer;
+        }
+
+        public void setAnswer(String answer) {
+            this.answer = answer;
+        }
+
+        public String getItem1() {
+            return item1;
+        }
+
+        public void setItem1(String item1) {
+            this.item1 = item1;
+        }
+
+        public String getItem2() {
+            return item2;
+        }
+
+        public void setItem2(String item2) {
+            this.item2 = item2;
+        }
+
+        public String getItem3() {
+            return item3;
+        }
+
+        public void setItem3(String item3) {
+            this.item3 = item3;
+        }
+
+        public String getItem4() {
+            return item4;
+        }
+
+        public void setItem4(String item4) {
+            this.item4 = item4;
+        }
+
+        public String getExplains() {
+            return explains;
+        }
+
+        public void setExplains(String explains) {
+            this.explains = explains;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+    }
+}
