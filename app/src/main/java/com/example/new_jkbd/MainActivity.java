9bc748f552f4ba247bef0b075ca146fc7a38cd75
969474010 <969474010@qq.com>
2017-07-04 20:28:01 +0800

    frist commit

---
+++ app/src/main/java/com/example/new_jkbd/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.new_jkbd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
