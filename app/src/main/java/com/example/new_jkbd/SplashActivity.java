37b0f83687422550918103a83a76a0eec157d43b
969474010 <969474010@qq.com>
2017-07-04 20:50:35 +0800

    设置两秒闪屏，并且进入主界面后不能再返回闪屏

--- app/src/main/java/com/example/new_jkbd/SplashActivity.java
+++ app/src/main/java/com/example/new_jkbd/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.example.new_jkbd;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +16,19 @@ public class SplashActivity extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        timer.start();
     }
+    CountDownTimer timer=new CountDownTimer(2000,1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
 
+        }
+
+        @Override
+        public void onFinish() {
+            Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
 }
