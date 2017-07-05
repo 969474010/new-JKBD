2a811232835a0fbac622daf3d2a602dbfab19120
969474010 <969474010@qq.com>
2017-07-05 16:40:44 +0800

    加载考题显示

--- app/src/main/java/com/example/new_jkbd/ExamApplication.java
+++ app/src/main/java/com/example/new_jkbd/ExamApplication.java
@@ -5,7 +5,9 @@ import android.util.Log;
 
 import com.example.new_jkbd.bean.Exam;
 import com.example.new_jkbd.bean.ExamInfo;
+import com.example.new_jkbd.bean.Result;
 import com.example.new_jkbd.utils.OkHttpUtils;
+import com.example.new_jkbd.utils.ResultUtils;
 
 import java.util.List;
 
@@ -48,6 +50,32 @@ public class ExamApplication extends Application {
                             @Override
                             public void onError(String error) {
                                 Log.e("main","error="+error);
+                            }
+                        });
+
+                OkHttpUtils<String> utils1=new OkHttpUtils<>(instance);
+                String url1="http://101.251.196.90:8080/JztkServer/getQuestions?testType=rand";
+                utils1.url(url1)
+                        .targetClass(String.class)
+                        .execute(new OkHttpUtils.OnCompleteListener<String>()
+                        {
+
+                            @Override
+                            public void onSuccess(String jsonStr) {
+                                Result result=ResultUtils.getListResultFromJson(jsonStr);
+                                if(result!=null&&result.getError_code()==0)
+                                {
+                                    List<Exam> list = result.getResult();
+                                    if(list!=null&&list.size()>0)
+                                    {
+                                        mExamList=list;
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onError(String error) {
+                                Log.e("mian","error="+error);
                             }
                         });
             }
--- app/src/main/java/com/example/new_jkbd/activity/ExamActivity.java
+++ app/src/main/java/com/example/new_jkbd/activity/ExamActivity.java
@@ -4,18 +4,23 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.new_jkbd.ExamApplication;
 import com.example.new_jkbd.R;
+import com.example.new_jkbd.bean.Exam;
 import com.example.new_jkbd.bean.ExamInfo;
+
+import java.util.List;
 
 /**
  * Created by &&&&& on 2017/7/4.
  */
 
 public class ExamActivity extends AppCompatActivity {
-    TextView tvExamInfo;
+    TextView tvExamInfo,tvExamTitle,tv_op1,tv_op2,tv_op3,tv_op4;
+    ImageView imageView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +30,14 @@ public class ExamActivity extends AppCompatActivity {
     }
 
     private void initView() {
+
         tvExamInfo= (TextView) findViewById(R.id.tv_examinfo);
+        tvExamTitle= (TextView) findViewById(R.id.tv_exam_title);
+        tv_op1= (TextView) findViewById(R.id.tv_op1);
+        tv_op2= (TextView) findViewById(R.id.tv_op2);
+        tv_op3= (TextView) findViewById(R.id.tv_op3);
+        tv_op4= (TextView) findViewById(R.id.tv_op4);
+        imageView= (ImageView) findViewById(R.id.im_exam_image);
     }
 
     private void initData() {
@@ -33,6 +45,23 @@ public class ExamActivity extends AppCompatActivity {
         if(examInfo!=null)
         {
             showData(examInfo);
+        }
+        List<Exam> examList=ExamApplication.getInstance().getmExamList();
+        if(examList!=null)
+        {
+            showExam(examList);
+        }
+    }
+
+    private void showExam(List<Exam> examList) {
+        Exam exam=examList.get(0);
+        if(exam!=null)
+        {
+            tvExamTitle.setText(exam.getQuestion());
+            tv_op1.setText(exam.getItem1());
+            tv_op2.setText(exam.getItem2());
+            tv_op3.setText(exam.getItem3());
+            tv_op4.setText(exam.getItem4());
         }
     }
 
