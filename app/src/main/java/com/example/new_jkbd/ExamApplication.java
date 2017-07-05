293e616be50fad7eba9eafee44405ab9559a3f9f
969474010 <969474010@qq.com>
2017-07-05 11:14:32 +0800

    下载考试信息

--- app/src/main/java/com/example/new_jkbd/ExamApplication.java
+++ app/src/main/java/com/example/new_jkbd/ExamApplication.java
@@ -1,19 +1,72 @@
 package com.example.new_jkbd;
 
 import android.app.Application;
+import android.util.Log;
+
+import com.example.new_jkbd.bean.Exam;
+import com.example.new_jkbd.bean.ExamInfo;
+import com.example.new_jkbd.utils.OkHttpUtils;
+
+import java.util.List;
 
 /**
  * Created by &&&&& on 2017/7/5.
  */
 
 public class ExamApplication extends Application {
+    ExamInfo mExamInfo;
+    List<Exam> mExamList;
+    private static ExamApplication instance;
     @Override
     public void onCreate() {
         super.onCreate();
+        instance=this;
         initData();
     }
 
+    public static ExamApplication getInstance()
+    {
+        return instance;
+    }
     private void initData() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpUtils<ExamInfo> utils=new OkHttpUtils<>(instance);
+                String url="http://101.251.196.90:8080/JztkServer/examInfo";
+                utils.url(url)
+                        .targetClass(ExamInfo.class)
+                        .execute(new OkHttpUtils.OnCompleteListener<ExamInfo>()
+                        {
 
+                            @Override
+                            public void onSuccess(ExamInfo result) {
+                                Log.e("main","result="+result);
+                                mExamInfo=result;
+                            }
+
+                            @Override
+                            public void onError(String error) {
+                                Log.e("main","error="+error);
+                            }
+                        });
+            }
+        }).start();
+    }
+
+    public ExamInfo getmExamInfo() {
+        return mExamInfo;
+    }
+
+    public void setmExamInfo(ExamInfo mExamInfo) {
+        this.mExamInfo = mExamInfo;
+    }
+
+    public List<Exam> getmExamList() {
+        return mExamList;
+    }
+
+    public void setmExamList(List<Exam> mExamList) {
+        this.mExamList = mExamList;
     }
 }
--- app/src/main/java/com/example/new_jkbd/activity/ExamActivity.java
+++ app/src/main/java/com/example/new_jkbd/activity/ExamActivity.java
@@ -4,18 +4,40 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import com.example.new_jkbd.ExamApplication;
 import com.example.new_jkbd.R;
+import com.example.new_jkbd.bean.ExamInfo;
 
 /**
  * Created by &&&&& on 2017/7/4.
  */
 
 public class ExamActivity extends AppCompatActivity {
-
+    TextView tvExamInfo;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam);
+        initView();
+        initData();
     }
+
+    private void initView() {
+        tvExamInfo= (TextView) findViewById(R.id.tv_examinfo);
+    }
+
+    private void initData() {
+        ExamInfo examInfo=ExamApplication.getInstance().getmExamInfo();
+        if(examInfo!=null)
+        {
+            showData(examInfo);
+        }
+    }
+
+    private void showData(ExamInfo examInfo) {
+        tvExamInfo.setText(examInfo.toString());
+    }
+
 }
